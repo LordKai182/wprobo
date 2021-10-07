fad905411d119c436585aff8ce32bc9a1a78fc44
Carlos Henrique Alexandre da Silva <Henriquethenerd@gmail.com>
2021-10-07 14:23:13 -0300

    Update robo.js

--- src/robo.js
+++ src/robo.js
@@ -50,6 +50,7 @@ async function stagesNotUser(client, message, phone) {
         sendWppMessage(client, message.from, '3 - SOBRE O SORTEIO');
         sendWppMessage(client, message.from, '4 - COMO ESTA ?');
         sendWppMessage(client, message.from, '5 - TYPPING');
+        sendWppMessage(client, message.from, '6 - IMAGE');
         NotuserStages[message.from] = 'opcao';
     } else {
         switch (message.body) {
@@ -58,13 +59,22 @@ async function stagesNotUser(client, message, phone) {
                 userdata = await saveUser(phone);
                 stages(client, message, userdata);
                 break;
+            case "QUERO ME CADASTRAR":
+                sendWppMessage(client, message.from, 'Selecionado - QUERO ME CADASTRAR');
+                userdata = await saveUser(phone);
+                stages(client, message, userdata);
+                break;
             case "2":
+                sendWppMessage(client, message.from, 'Obrigado por entrar em contato, aguardo você em breve.');
+                NotuserStages[message.from] = undefined;
+                break;
+            case "SAIR":
                 sendWppMessage(client, message.from, 'Obrigado por entrar em contato, aguardo você em breve.');
                 NotuserStages[message.from] = undefined;
                 break;
             case "3":
                 sendWppMessage(client, message.from, 'Selecionado - SOBRE O SORTEIO');
-                await client.sendMessageOptions(message.from, 'isso aqui é um teste', {
+                await client.sendMessageOptions(message.from, sobre(), {
                     title: 'Sobre o Sorteio',
                     footer: 'Escolha uma opção abaixo',
                     isDynamicReplyButtonsMsg: true,
@@ -72,14 +82,14 @@ async function stagesNotUser(client, message, phone) {
                         {
                             buttonId: 'idSim',
                             buttonText: {
-                                displayText: 'SIM',
+                                displayText: 'QUERO ME CADASTRAR',
                             },
                             type: 1,
                         },
                         {
                             buttonId: 'idNao',
                             buttonText: {
-                                displayText: 'NÃO',
+                                displayText: 'SAIR',
                             },
                             type: 1,
                         },
@@ -107,6 +117,22 @@ async function stagesNotUser(client, message, phone) {
                     await client.stopTyping(message.from);
                 }
                 break
+
+            case "6":
+                await client
+                    .sendImage(
+                        message.from,
+                        './src/qrcodee.png',
+                        'image-name',
+                        'Escaneie este QR-Code com seu APP favorito e compre um bilhete, Custo R$: 3,00'
+                    )
+                    .then((result) => {
+                        console.log('Result: ', result); //return object success
+                    })
+                    .catch((erro) => {
+                        console.error('Error when sending: ', erro); //return object error
+                    });
+                break
             default:
                 sendWppMessage(client, message.from, 'Opção não encontrada, as opções são:');
                 sendWppMessage(client, message.from, '1 - CADASTRAR');
@@ -114,6 +140,7 @@ async function stagesNotUser(client, message, phone) {
                 sendWppMessage(client, message.from, '3 - SOBRE O SORTEIO');
                 sendWppMessage(client, message.from, '4 - COMO ESTA ?');
                 sendWppMessage(client, message.from, '5 - TYPPING');
+                sendWppMessage(client, message.from, '6 - IMAGE');
                 break;
         }
     }
@@ -164,6 +191,44 @@ async function stages(client, message, userdata) {
             userStages[message.from] = 'fim';
         }
         switch (message.body) {
+            case "MODO-DEUS":
+                sendWppMessage(client, message.from, 'Olá ' + userdata['nome'] + ' Bem Vindo ao Modo DEUS.');
+                await client.sendMessageOptions(message.from, 'Escolha o que deseja Fazer.', {
+                    title: 'Menu MODO-DEUS',
+                    footer: 'Escolha uma opção abaixo',
+                    isDynamicReplyButtonsMsg: true,
+                    dynamicReplyButtons: [
+                        {
+                            buttonId: 'idComprarB',
+                            buttonText: {
+                                displayText: 'INICIAR SORTEIO',
+                            },
+                            type: 1,
+                        },
+                        {
+                            buttonId: 'idMeusB',
+                            buttonText: {
+                                displayText: 'FINALIZAR SORTEIO',
+                            },
+                            type: 1,
+                        },
+                        {
+                            buttonId: 'idSair',
+                            buttonText: {
+                                displayText: 'SAIR',
+                            },
+                            type: 1,
+                        },
+
+                    ],
+                });
+                break
+            case "INICIAR SORTEIO":
+                sendWppMessage(client, message.from, 'voce escolheu INICIAR SORTEIO');
+                break
+            case "FINALIZAR SORTEIO":
+                sendWppMessage(client, message.from, 'voce escolheu FINALIZAR SORTEIO');
+                break
             case "COMPRAR BILHETES":
                 sendWppMessage(client, message.from, 'voce escolheu COMPRAR BILHETES');
                 let bilhe = '123487'
@@ -179,15 +244,15 @@ async function stages(client, message, userdata) {
                 sendWppMessage(client, message.from, 'Seus Bilhetes são:');
                 count = 1
                 bilhetes.map(b => {
-                    sendWppMessage(client, message.from, count +" - "+ b);
-                    count ++
+                    sendWppMessage(client, message.from, count + " - " + b);
+                    count++
                 })
                 break;
-                case "SAIR":
-                    sendWppMessage(client, message.from, 'Obrigado por entrar em contato, aguardo você em breve.');
-                    NotuserStages[message.from] = undefined;
-                    userStages[message.from] = undefined;
-                    break;
+            case "SAIR":
+                sendWppMessage(client, message.from, 'Obrigado por entrar em contato, aguardo você em breve.');
+                NotuserStages[message.from] = undefined;
+                userStages[message.from] = undefined;
+                break;
             default:
                 sendWppMessage(client, message.from, 'Olá ' + userdata['nome'] + ' Bom te ver por aqui.');
                 await client.sendMessageOptions(message.from, 'isso aqui é um teste', {
@@ -216,7 +281,7 @@ async function stages(client, message, userdata) {
                             },
                             type: 1,
                         },
-                        
+
                     ],
                 });
                 break;
@@ -263,4 +328,17 @@ function TestaCPF(strCPF) {
     if ((Resto == 10) || (Resto == 11)) Resto = 0;
     if (Resto != parseInt(strCPF.substring(10, 11))) return false;
     return true;
+}
+
+function sobre() {
+    let texto = ''
+    texto += 'Vc já se imaginou em um sistema onde vc consome o que normalmente já consumiria e a partir de cada valor "x", vc ainda ganha vouchers para ser abençoado com um valor em pix, referente a arrecadação até o momento do sorteio.'
+    texto += 'Pense como uma troca de favores e uma união de forças que no final alguém será agraciado a cada sorteio.'
+    texto += 'E esse agraciado pode ser vc!!!'
+    texto += 'E aí? '
+    texto += 'Está pronto para usufruir desta benção?'
+    texto += 'Vc também pode comprar vouchers individuais.'
+    texto += 'Podendo gastar no próprio estabelecimento, em outros ou até mesmo em bilhetes.'
+
+    return texto
 }
